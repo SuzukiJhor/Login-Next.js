1b16918f8a4bb7344a368e320431be6d82af1a39
Jhordan S <jhordan.kendy@outlook.com>
2023-11-22 14:16:14 -0300

    Feat: add credentials .

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,7 @@
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
+
 export const options: NextAuthOptions = {
     providers: [
         GithubProvider({}),
